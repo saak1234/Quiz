f17fbc1cbfb94b8c9842cf7c80ee18bb60182341
Akshoy <akshoygorai33@gmail.com>
2024-06-17 00:04:47 +0530

    Updated vercel.json

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 app.use(cors())
-
 app.use(express.json()) 
 app.use(Router)
-mongoose.connect('mongodb+srv://akshoygorai33:HstZQ9W8Q16zMcTX@cluster0.fph0itf.mongodb.net/')
+mongoose.connect('mongodb+srv://akshoygorai33:NovY0c04G6l4HyFJ@cluster0.fph0itf.mongodb.net/')
 app.listen(8080, () => {
     console.log("Server is up on port 8080")
 })
